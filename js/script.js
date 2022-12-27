68ea7714c5506fccc167bcf01237f52a9e056875
Oksanka <zazera777@gmail.com>
2022-12-27 20:16:31 +0200

    add js and script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 var tweetURL = "";
-  var tempList = "";
-  var randomQuote = "";
-  var html = "";
-  var quoteBody = document.querySelector("#quote-body");
-  var quote = document.querySelector("#quote-text");
-  var button = document.querySelector("#quote-btn");
+var tempList = "";
+var randomQuote = "";
+var html = "";
+var quoteBody = document.querySelector("#quote-body");
+var quote = document.querySelector("#quote-text");
+var button = document.querySelector("#quote-btn");
 var share = document.querySelector("#twitter");
-  
+
 // Here Object with Quotes & Authors
 var quotes = [
-      
-  ]
+    {
+        text: "Most of the good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.",
+        author: "— Linus Torvalds"
+    },
+];
