576ad650eaa996973334f14652111d57abc12793
Oksanka <zazera777@gmail.com>
2022-12-27 20:11:52 +0200

    add js and script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var tweetURL = "";
+  var tempList = "";
+  var randomQuote = "";
+  var html = "";
+  var quoteBody = document.querySelector("#quote-body");
+  var quote = document.querySelector("#quote-text");
+  var button = document.querySelector("#quote-btn");
+var share = document.querySelector("#twitter");
+  
+// Here Object with Quotes & Authors
+var quotes = [
+      
+  ]
