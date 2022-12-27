429f02c0ebe0eb7cc74bac2d1ce3ffb53b587ab8
Oksanka <zazera777@gmail.com>
2022-12-27 20:18:57 +0200

    add js and script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ var share = document.querySelector("#twitter");
 var quotes = [
     {
         text: "Most of the good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.",
-        author: "— Linus Torvalds"
+        author: "— Linus Torvalds —"
     },
+    {
+        text: "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
+        author: "— Edsger Dijkstra —"
+    }
 ];
