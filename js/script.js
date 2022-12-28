51014a3ba6bbe5a7e195639fd84e291107d6ba1b
Oksanka <zazera777@gmail.com>
2022-12-28 09:56:34 +0200

    add js and script.js

--- js/script.js
+++ js/script.js
@@ -118,304 +118,155 @@ var quotes = [
     author: "— Cameron Diaz —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Electricity can transform people's lives, not just economically but also socially.",
+    author: "— Piyush Goya —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Electricity is really just organized lightning.",
+    author: "— George Carlin —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "We can, and must, shift to an economy in which 100% of our electricity is generated renewably.",
+    author: "— Jill Stein —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Why is electricity so expensive these days? Why does it cost so much for something I can make with a balloon and my hair?",
+    author: "— Dennis Miller —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Once electricity and petrol and facilities and food stop, society stops. It's crazy.",
+    author: "— Alycia Debnam-Carey —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "5G will have an impact similar to the introduction of electricity or the car, affecting entire economies and benefiting entire societies.",
+    author: "— Steve Mollenkopf —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Imagination has brought mankind through the dark ages to its present state of civilization. Imagination led Columbus to discover America. Imagination led Franklin to discover electricity.",
+    author: "— L. Frank Baum —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I'm not a techie, but I don't know how I lived without an iPad! Mine comes with me everywhere. As greatest inventions go, it's up there with electricity and cars.",
+    author: "— Joanna Garcia —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Ampere was the Newton of Electricity.",
+    author: "— James C. Maxwell —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "We should learn to live more with our climate and rely less on electricity to alter our climate.",
+    author: "— James Dyson —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I don't have a crystal ball, but I'm willing to bet one of my arms right now that as long as there's electricity, Ramones music is going to be relevant.",
+    author: "— Henry Rollins —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "My grandmother's house - she ran it just like her grandmother and her great-grandmother. They didn't have electricity. They had wood stoves that never got cold.",
+    author: "— Willard Scott —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Electricity, the peril the wind sings to in the wires on a gray day.",
+    author: "— Janet Frame —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "For as long as anyone can remember, reliable, cheap electricity has been taken for granted in the United States.",
+    author: "— Alex Berenson —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "We're electrical items and when we die the electricity goes somewhere else. When we die our energy goes into the galaxy.",
+    author: "— Dominic Monaghan —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Our increasingly electrified, electronic, and data-driven society places steadily rising demand on reliable baseload power - that is, on electricity available 24/7/365. Servers never sleep, nor does air conditioning during hot nights, and in Asia's megacities, subways and electric trains take only brief naps between midnight and 5 A.M.",
+    author: "— Vaclav Smil —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "When I meet a person and that chemistry is there, I cannot hide the electricity. I need to learn more about him, and once I feel safe, I'm gone, I'm in love, and I give it my all!",
+    author: "— Taraji P. Henson —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I fell in love with electricity.",
+    author: "— Geisha Williams —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "To avoid a blackout, electricity generating companies must lower generation in line with low demand.",
+    author: "— Jyotiraditya Madhavrao Scindia —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I know a lot of people around the world have these problems where there's just no light, no electricity, nothing.",
+    author: "— Ann Makosinski —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "The first time I acted was in high school in Florida, and when I heard that applause I felt so alive and felt that electricity go up my spine.",
+    author: "— Angela Bassett —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Electricity is derived from many non-renewable energy sources like oil, natural gas and coal.",
+    author: "— Lamar S. Smith —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Fibre optic is becoming like electricity. If you look at how electricity spread around the globe 100 years ago, that's what's happening now.",
+    author: "— Reed Hastings —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I grew up in rural Oregon in a log house with bark left on inside and out. We had no electricity, a massive stone fireplace, a grand piano, and tons of books.",
+    author: "— Virginia Euwer Wolff —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "When people listen to Jay-Z, they're working all day or trying to work and pay their bills, and what they hear is someone who's free. Who doesn't have to worry about the electricity. But all we're taught is that those who are rich deserve to be rich because they worked harder than the rest of us, or they're smarter.",
+    author: "— Boots Riley —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I agree completely with my son James when he says 'Internet is like electricity. The latter lights up everything, while the former lights up knowledge'.",
+    author: "— Kerry Packer —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "The air is dirty because of the electricity monopolists. They have powerful money lobbyists.",
+    author: "— Sam Wyly —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "In the early 1980s, I got into a war with my management - they just kept on suing me and I lost everything. So I had to go out on tour to make sure the electricity stayed on.",
+    author: "— Meat Loaf —",
   },
+  
   {
-    text: "  ",
-    author: "—  —",
+    text: "Only electricity can give the transport sector the flexibility to switch fuels when one or more become too expensive.",
+    author: "— Frederick W. Smith —",
   },
+  
   {
-    text: "  ",
-    author: "—  —",
+    text: "While there is an electricity, we need to work",
+    author: "— Oksana Kolisnyk (Author, Ukraine, 12.2022) —",
   },
+
   {
-    text: "  ",
-    author: "—  —",
+    text: "They can't do without electricity. They can do with less electricity.",
+    author: "— Kenneth Lay —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "I am an expert of electricity. My father occupied the chair of applied electricity at the state prison.",
+    author: "— W. C. Fields —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Ben Franklin may have discovered electricity- but it is the man who invented the meter who made the money.",
+    author: "— Earl Warren —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "What is a soul? It's like electricity - we don't really know what it is, but it's a force that can light a room.",
+    author: "— Ray Charles —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "We take for granted electricity, water, even concerts. Count your blessings.",
+    author: "— Damian Marley —",
   },
   {
-    text: "  ",
-    author: "—  —",
+    text: "Without electricity, the air would rot.",
+    author: "— Ralph Waldo Emerson —",
   },
   {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
-  },
-  {
-    text: "  ",
-    author: "—  —",
+    text: " The kingdom of heaven is like electricity. You don't see it. It is within you.",
+    author: "— Maharishi Mahesh Yogi —",
   },
 ];
 
