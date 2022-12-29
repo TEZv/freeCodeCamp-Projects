4217940ee8eacd5585d3d904a4b4d176fb04a485
TEZv <zazera777@gmail.com>
2022-12-29 17:14:59 +0200

    add script.jsx

--- frontEndCertificate/markdownPreviewer/script.jsx
+++ frontEndCertificate/markdownPreviewer/script.jsx
@@ -1,38 +1,42 @@
 class MarkupArea extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            textValue: 'HeadingOne\n=======\n \n# HeadingTwo\n \nSub-headingOne\n-----------\n\
+  constructor() {
+    super();
+    this.state = {
+      textValue:
+        "HeadingOne\n=======\n \n# HeadingTwo\n \nSub-headingOne\n-----------\n\
             \n## Sub-headingTwo\n \n### A smaller heading\n \nParagraphs are separated by a blank line.\n\
             \nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes: *bold*, **italic**,\
             `monospace`, ~~strikethrough~~ .\n\n Unordered list (useof asterisks):\n\n  * butter\n  * sugar\n  * flour\n\
             \nOrdered list (useof numbers):\n\n  1. Pre-heat over\n  2. Mix ingredients\n  3. Bake for 60 minutes\n\
             \nFor links only use brackets and parentheses []()  \
-            \n[Github Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)' }
-        
-        this.setText = this.setText.bind(this);
-    this.output= this.output.bind(this);
-    }
-    
-    setText(payload) {
-    this.setState({textValue: payload.target.value});
-    }
-    
-    output() {
-   return {__html: marked(this.state.textValue)};
-    } 
-    
-    render() {
+            \n[Github Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)",
+    };
+
+    this.setText = this.setText.bind(this);
+    this.output = this.output.bind(this);
+  }
+
+  setText(payload) {
+    this.setState({ textValue: payload.target.value });
+  }
+
+  output() {
+    return { __html: marked(this.state.textValue) };
+  }
+
+  render() {
     return (
-      <div id = 'editor'>
-        <div id= 'preview'>
-          <h1 className ='headings'>Markup:</h1>
-          <textarea value = {this.state.textValue} onChange = {this.setText} >
-          </textarea>
+      <div id="editor">
+        <div id="preview">
+          <h1 className="headings">Markup:</h1>
+          <textarea
+            value={this.state.textValue}
+            onChange={this.setText}
+          ></textarea>
         </div>
-        <div id = 'output'>
-          <h1 className='headings'>Output:</h1>
-          <span dangerouslySetInnerHTML = {this.output()}></span>
+        <div id="output">
+          <h1 className="headings">Output:</h1>
+          <span dangerouslySetInnerHTML={this.output()}></span>
         </div>
       </div>
     );
@@ -40,16 +44,20 @@ class MarkupArea extends React.Component {
 }
 
 const Footer = () => (
-    <footer>
-    <p><a href = 'https://github.com/ChrisCurl/Markdown-Previewer'>Chris Curl March 2018</a></p>
-    </footer>
+  <footer>
+    <p>
+      <a href="https://github.com/ChrisCurl/Markdown-Previewer">
+        Chris Curl March 2018
+      </a>
+    </p>
+  </footer>
 );
 
 const App = () => (
-  <div id = 'app'>
+  <div id="app">
     <MarkupArea />
     <Footer />
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
