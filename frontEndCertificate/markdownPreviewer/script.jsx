3be45014acf317efa1033af1ce614984368441c5
TEZv <zazera777@gmail.com>
2022-12-29 17:07:28 +0200

    add script.jsx

--- frontEndCertificate/markdownPreviewer/script.jsx
+++ frontEndCertificate/markdownPreviewer/script.jsx
@@ -2,5 +2,7 @@ class MarkupArea extends React.Component {
     constructor() {
         super()
         this.state = {
-            textValue: 'HeadingOne\n=======\n \n# HeadingTwo\n \nSub-heading\n-----------\n\
-            
+            textValue: 'HeadingOne\n=======\n \n# HeadingTwo\n \nSub-headingOne\n-----------\n\
+            \n## Sub-headingTwo\n \n### A smaller heading\n \nParagraphs are separated by a blank line.\n\
+
+        }
