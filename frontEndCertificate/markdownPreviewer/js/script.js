879e3496e7d9bdb70ce4dce5588029c6a234ac80
TEZv <zazera777@gmail.com>
2022-12-29 21:32:40 +0200

    add js and script.js

--- frontEndCertificate/markdownPreviewer/js/script.js
+++ frontEndCertificate/markdownPreviewer/js/script.js
@@ -13,3 +13,16 @@ const renderer = new marked.Renderer();
 renderer.link = function (href, title, text) {
   return `<a target="_blank" href="${href}">${text}</a>`;
 };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+      editorMaximized: false,
+      previewMaximized: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
+  }
