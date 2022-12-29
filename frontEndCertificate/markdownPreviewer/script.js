97e39cac4fbe30ec85de08b2930da346c5af9f03
TEZv <zazera777@gmail.com>
2022-12-29 21:29:54 +0200

    add script.js

---
+++ frontEndCertificate/markdownPreviewer/script.js
@@ -0,0 +1,15 @@
+const projectName = 'markdown-previewer';
+
+// ALLOWS LINE BREAKS WITH RETURN BUTTON
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  } });
+
+
+// INSERTS target="_blank" INTO HREF TAGS (required for Codepen links)
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
