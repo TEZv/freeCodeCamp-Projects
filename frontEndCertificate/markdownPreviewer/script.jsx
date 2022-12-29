312dfa5353e7da3cdbe6cc73c00add5828c018a5
TEZv <zazera777@gmail.com>
2022-12-29 17:08:41 +0200

    add script.jsx

--- frontEndCertificate/markdownPreviewer/script.jsx
+++ frontEndCertificate/markdownPreviewer/script.jsx
@@ -4,5 +4,6 @@ class MarkupArea extends React.Component {
         this.state = {
             textValue: 'HeadingOne\n=======\n \n# HeadingTwo\n \nSub-headingOne\n-----------\n\
             \n## Sub-headingTwo\n \n### A smaller heading\n \nParagraphs are separated by a blank line.\n\
-
+            \nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes: *bold*, **italic**,\
+            `monospace`, ~~strikethrough~~ .\n\n Unordered list (use asterisks):\n\n  * butter\n  * sugar\n  * flour\n\
         }
