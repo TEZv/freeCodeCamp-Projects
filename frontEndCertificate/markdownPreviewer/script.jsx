9ccda9093e9eebaa2d85dba0766d49db24940543
TEZv <zazera777@gmail.com>
2022-12-29 17:12:11 +0200

    add script.jsx

--- frontEndCertificate/markdownPreviewer/script.jsx
+++ frontEndCertificate/markdownPreviewer/script.jsx
@@ -6,4 +6,18 @@ class MarkupArea extends React.Component {
             \n## Sub-headingTwo\n \n### A smaller heading\n \nParagraphs are separated by a blank line.\n\
             \nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes: *bold*, **italic**,\
             `monospace`, ~~strikethrough~~ .\n\n Unordered list (useof asterisks):\n\n  * butter\n  * sugar\n  * flour\n\
-        }
+            \nOrdered list (useof numbers):\n\n  1. Pre-heat over\n  2. Mix ingredients\n  3. Bake for 60 minutes\n\
+            \nFor links only use brackets and parentheses []()  \
+            \n[Github Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)' }
+        
+        this.setText = this.setText.bind(this);
+    this.output= this.output.bind(this);
+    }
+    
+    setText(payload) {
+    this.setState({textValue: payload.target.value});
+    }
+    
+    output() {
+   return {__html: marked(this.state.textValue)};
+  } 
