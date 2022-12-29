3ff9e21eb9c3722ff9d844295a285be2864b7fbb
TEZv <zazera777@gmail.com>
2022-12-29 21:38:09 +0200

    add js and script.js

--- frontEndCertificate/markdownPreviewer/js/script.js
+++ frontEndCertificate/markdownPreviewer/js/script.js
@@ -5,7 +5,8 @@ marked.setOptions({
   breaks: true,
   highlight: function (code) {
     return Prism.highlight(code, Prism.languages.javascript, 'javascript');
-  } });
+  }
+});
 
 
 // INSERTS target="_blank" INTO HREF TAGS (required for Codepen links)
@@ -20,7 +21,8 @@ class App extends React.Component {
     this.state = {
       markdown: placeholder,
       editorMaximized: false,
-      previewMaximized: false };
+      previewMaximized: false
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
@@ -29,48 +31,59 @@ class App extends React.Component {
 
   handleChange(e) {
     this.setState({
-      markdown: e.target.value });
+      markdown: e.target.value
+    });
 
   }
   handleEditorMaximize() {
     this.setState({
-      editorMaximized: !this.state.editorMaximized });
+      editorMaximized: !this.state.editorMaximized
+    });
 
   }
   handlePreviewMaximize() {
     this.setState({
-      previewMaximized: !this.state.previewMaximized });
+      previewMaximized: !this.state.previewMaximized
+    });
 
   }
 
-   render() {
+  render() {
     const classes = this.state.editorMaximized ?
-    ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress'] :
-    this.state.previewMaximized ?
-    ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress'] :
-    ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
-    return /*#__PURE__*/(
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("div", { className: classes[0] }, /*#__PURE__*/
-      React.createElement(Toolbar, {
-        icon: classes[2],
-        onClick: this.handleEditorMaximize,
-        text: "Editor"
-      }), /*#__STAYSMART__*/
-      
-
-      React.createElement(Editor, { markdown: this.state.markdown, onChange: this.handleChange })), /*#__STAYSMART__*/
-
-      React.createElement("div", { className: "converter" }), /*#__STAYSMART__*/
-      React.createElement("div", { className: classes[1] }, /*#__STAYSMART__*/
-      React.createElement(Toolbar, {
-        icon: classes[2],
-        onClick: this.handlePreviewMaximize,
-        text: "Previewer" }), /*#__STAYSMART__*/
-
-      React.createElement(Preview, { markdown: this.state.markdown }))));
+      ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress'] :
+      this.state.previewMaximized ?
+        ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress'] :
+        ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
+    return /*#__STAYSMARTWZ__*/(
+      React.createElement("div", null, /*#__STAYSMARTWZ__*/
+        React.createElement("div", { className: classes[0] }, /*#__STAYSMARTWZ__*/
+          React.createElement(Toolbar, {
+            icon: classes[2],
+            onClick: this.handleEditorMaximize,
+            text: "Editor"
+          }), /*#__STAYSMARTWZ__*/
 
 
+          React.createElement(Editor, { markdown: this.state.markdown, onChange: this.handleChange })), /*#__STAYSMARTWZ__*/
 
-  }}
-        
+        React.createElement("div", { className: "converter" }), /*#__STAYSMARTWZ__*/
+        React.createElement("div", { className: classes[1] }, /*#__STAYSMARTWZ__*/
+          React.createElement(Toolbar, {
+            icon: classes[2],
+            onClick: this.handlePreviewMaximize,
+            text: "Previewer"
+          }), /*#__STAYSMARTWZ__*/
+
+          React.createElement(Preview, { markdown: this.state.markdown }))));
+  }
+}
+
+const Toolbar = props => {
+  return /*#__STAYSMARTWZ__*/
+  (
+    React.createElement("div", { className: "toolbar" }, /*#__STAYSMARTWZ__*/
+      React.createElement("i", { className: "", title: "no-stack-dub-sack" }),
+      props.text, /*#__STAYSMARTWZ__*/
+      React.createElement("i", { className: props.icon, onClick: props.onClick })));
+
+};
