ad6e79362483ae3275e8d1513395a8f1b113d186
TEZv <zazera777@gmail.com>
2022-12-30 19:34:09 +0200

    add js and script.js

---
+++ frontEndCertificate/drumMachine/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d000",
+    "#e380e0",
+    "#2260a3",
+    "#d0d460",
+    "#f1aaca",
+    "#afb1c0",
+  ];
+}
